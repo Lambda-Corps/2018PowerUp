0af25e3b73398debf9c697ce00ff7808f40279aa
mseputro1 <mseputro1220@gmail.com>
2018-01-16 19:48:03 -0500

    Project has updated Talon Libraries now. I added the current limiting code to one motor (left_dt_motor1).

--- src/org/usfirst/frc/team1895/robot/subsystems/Drivetrain.java
+++ src/org/usfirst/frc/team1895/robot/subsystems/Drivetrain.java
@@ -1,12 +1,12 @@
 package org.usfirst.frc.team1895.robot.subsystems;
 
-import com.ctre.TalonSRX;
+import org.usfirst.frc.team1895.robot.RobotMap;
 
+import com.ctre.phoenix.motorcontrol.can.*;
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
-import edu.wpi.first.wpilibj.TalonSRX;
 import edu.wpi.first.wpilibj.command.Subsystem;
-
+import com.ctre.phoenix.motorcontrol.*;
 /**
  * Function: Drives around. Can gear shift into high and low gear. I am also including the Compressor here. 
  * Motors: 2 pistons (transmission), 4 motors (wheels)
@@ -28,11 +28,27 @@ public class Drivetrain extends Subsystem {
     
     public Drivetrain() {
     	//motors
+    	left_dt_motor1 = new TalonSRX(RobotMap.LEFT_DT_MOTOR1_PORT);
+    	left_dt_motor2 = new TalonSRX(RobotMap.LEFT_DT_MOTOR2_PORT);
+    	right_dt_motor1 = new TalonSRX(RobotMap.RIGHT_DT_MOTOR1_PORT);
+    	right_dt_motor2 = new TalonSRX(RobotMap.RIGHT_DT_MOTOR2_PORT);
+    	
+    	//current limited to 10 amps when current is >15amps for 100 milliseconds
+    	left_dt_motor1.configContinuousCurrentLimit(10, 0); 
+    	left_dt_motor1.configPeakCurrentLimit(15,  0);
+    	left_dt_motor1.configPeakCurrentDuration(100, 0);
+    	left_dt_motor1.enableCurrentLimit(true);
+    	
+    	left_dt_motor2.set(ControlMode.Follower, 6); 
     	
     	//pneumatics
-    	
+    	compressor = new Compressor();
+    	transmission_solenoid = new DoubleSolenoid(RobotMap.DRIVETRAIN_SOLENOID_A_PORT, RobotMap.DRIVETRAIN_SOLENOID_B_PORT);
     }
 
+    // Testing current limiting
+    
+    
 	public void initDefaultCommand() {
 		// Set the default command for a subsystem here.
 		// setDefaultCommand(new MySpecialCommand());
