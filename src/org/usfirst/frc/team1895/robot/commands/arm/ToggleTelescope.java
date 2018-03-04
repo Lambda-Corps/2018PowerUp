10a36e894131b207bbb1737738df531f0c054037
nl2 <nl2_email>
2018-03-04 12:39:49 -0500

    Final match from battlefield event.

---
+++ src/org/usfirst/frc/team1895/robot/commands/arm/ToggleTelescope.java
@@ -0,0 +1,44 @@
+package org.usfirst.frc.team1895.robot.commands.arm;
+
+import org.usfirst.frc.team1895.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class ToggleTelescope extends Command {
+
+	int count = 0;
+	boolean done = false;
+    public ToggleTelescope() {
+        // Use requires() here to declare subsystem dependencies
+        requires(Robot.arm);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	Robot.arm.toggleTelescopingArm();
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	if(count++ == 30) {
+    		done = true;
+    	}
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return done;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
---
+++ src/org/usfirst/frc/team1895/robot/commands/lowerIntake/ToggleLowerIntake.java
@@ -0,0 +1,47 @@
+package org.usfirst.frc.team1895.robot.commands.lowerIntake;
+
+import org.usfirst.frc.team1895.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class ToggleLowerIntake extends Command {
+
+	int counter;
+	boolean done;
+    public ToggleLowerIntake() {
+        // Use requires() here to declare subsystem dependencies
+        requires(Robot.lowerIntake);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	counter = 0;
+    	done = false;
+    	Robot.lowerIntake.toggleLowerIntake();
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	if(counter>=30) {
+    		done = true;
+    	}
+    	counter++;
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return done;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
